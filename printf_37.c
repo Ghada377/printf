c7bbd0bbae5b55921b02cfc1017f3528be1cbe01
RanaHany265 <ranahanymohamed265@gmail.com>
2023-10-17 18:30:20 -0700

    task2

---
+++ printf_37.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+ * print_37 - prints the char 37.
+ * * @args: input
+ * Return: 1.
+ */
+int print_37(va_list args)
+{
+	(void)args;
+	_putchar(37);
+	return (1);
+}
+
